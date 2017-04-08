9e37eb0b39857776984af22855fac7c56e6112cf
janzeteachesit <janzeteachesit@gmail.com>
2017-04-07 20:03:30 -0700

    Update raycaster.js

--- docs/raycaster/raycaster.js
+++ docs/raycaster/raycaster.js
@@ -5,6 +5,7 @@ and https://github.com/hunterloftis/playfuljs-demos/blob/gh-pages/raycaster/inde
 
 */
 
+ var canvas = document.createElement("canvas");
  var CIRCLE = Math.PI * 2;
  var MOBILE = /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)
 
